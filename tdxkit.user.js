680bebdfcd198a3ac98283cff956474a12d28f7e
Hunter Fuller <hfuller@pixilic.com>
2019-02-08 22:49:28 -0600

    TDXKit v3: Handle popped windows from deep link
    If a deep link required a login, TDX will pop the window up
    after the login completes. We detect this and close the
    window and move its contents into the main window.

--- tdxkit.user.js
+++ tdxkit.user.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name        TDXKit
+// @author      Hunter Fuller <hf0002@uah.edu>
 // @description Adds some opinionated improvements to TeamDynamix
-// @version     2
+// @version     3
 // @updateURL   https://github.com/hfuller/uah-user-js/raw/master/tdxkit.user.js
 // @downloadURL https://github.com/hfuller/uah-user-js/raw/master/tdxkit.user.js
 // @namespace   https://github.com/hfuller/uah-user-js
 
 // @grant       none
 // @include     https://*.teamdynamix.com/TDNext/*
+
+// @history     3 If TDX pops a window using a ToUrl deep link (post-login), close it and go there in-window instead
+// @history     2 Replace all links that just run openWin() with direct links
+// @history     1 Patch openWin to not actually open a window
 // ==/UserScript==
 
 console.log("TDXKit loaded, adding indicator.");
@@ -49,4 +54,15 @@ for ( let a of document.getElementsByTagName('a') ) {
       console.log("Couldn't fixup " + a.innerHTML + " " + a.href);
     }
   }
+}
+
+console.log("Checking for ToUrl popped window");
+if ( document.referrer.includes("ToUrl=") ) {
+    console.log("This is a ToUrl window. Closing the window and moving its contents here.");
+    let popup = window.open("", "_NewWindow"); //magic string TDX opens windows with
+    //let dest = popup.location.href;
+    popup.close();
+    let thing = document.referrer.split("?ToUrl=");
+    let dest = decodeURIComponent(thing[1]);
+    window.location = dest;
 }
