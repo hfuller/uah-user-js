88ac6d72af5d67ff812db6e650e636de905f8616
Hunter Fuller <hfuller@pixilic.com>
2020-08-06 13:04:47 -0500

    HealthCheck Fixer v2: it actually works now.

--- HealthCheck_Fixer.user.js
+++ HealthCheck_Fixer.user.js
@@ -4,13 +4,14 @@
 // @version      1
 // @description  Don't ask me for my phone number
 // @author       Hunter Fuller <hf0002@uah.edu>
-// @updateURL   https://github.com/hfuller/uah-user-js/raw/master/HealthCheck_Fixer.user.js
-// @downloadURL https://github.com/hfuller/uah-user-js/raw/master/HealthCheck_Fixer.user.js
+// @updateURL    https://github.com/hfuller/uah-user-js/raw/master/HealthCheck_Fixer.user.js
+// @downloadURL  https://github.com/hfuller/uah-user-js/raw/master/HealthCheck_Fixer.user.js
 
 // @include      https://healthcheck.staysafetogether.org/40?*
 // @grant        none
 
-// @history     1 When asked for my phone number, just click next.
+// @history      2 Check whether the page is displayed and skip it only then
+// @history      1 When asked for my phone number, just click next.
 
 // ==/UserScript==
 
@@ -18,7 +19,8 @@
     'use strict';
 
     window.setInterval(function() {
-        if ( document.getElementById("tfa_1-L").innerHTML.includes("What is your phone number") ) {
+        if ( document.getElementById("tfa_1-L").innerHTML.includes("What is your phone number") && document.getElementById("wfPgIndex-1").classList.contains("wfCurrentPage") ) {
+            //page 1 contains the text and we are on page 1
             document.getElementById("wfPageNextId1").click();
         }
     }, 1000);
