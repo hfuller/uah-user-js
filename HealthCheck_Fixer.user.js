5ff75f1078d9947a4f7f24964481b14f424f06f9
Hunter Fuller <hfuller@pixilic.com>
2021-03-07 21:16:11 -0600

    HealthCheck Fixer v3: Fill in the form (without submitting)

--- HealthCheck_Fixer.user.js
+++ HealthCheck_Fixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HealthCheck Fixer
 // @namespace    https://github.com/hfuller/uah-user-js
-// @version      1
+// @version      3
 // @description  Don't ask me for my phone number
 // @author       Hunter Fuller <hf0002@uah.edu>
 // @updateURL    https://github.com/hfuller/uah-user-js/raw/master/HealthCheck_Fixer.user.js
@@ -10,6 +10,7 @@
 // @include      https://healthcheck.staysafetogether.org/40?*
 // @grant        none
 
+// @history      3 Fill in the form (without submitting)
 // @history      2 Check whether the page is displayed and skip it only then
 // @history      1 When asked for my phone number, just click next.
 
@@ -22,6 +23,9 @@
         if ( document.getElementById("tfa_1-L").innerHTML.includes("What is your phone number") && document.getElementById("wfPgIndex-1").classList.contains("wfCurrentPage") ) {
             //page 1 contains the text and we are on page 1
             document.getElementById("wfPageNextId1").click();
+            for ( let x of [5, 10, 41, 75] ) {
+                document.getElementById("tfa_" + x + "-L").click();
+            }
         }
     }, 1000);
 })();
